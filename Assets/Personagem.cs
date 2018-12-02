67eeda264cd34965f9acfd21561396eabf0c80bd
eliandroivan <eliandroivan@gmail.com>
2018-12-02 16:04:41 -0200

    jogo upath

---
+++ Assets/Personagem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personagem : MonoBehaviour {
+
+	public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveY = Input.GetAxis("Vertical") * speed;
+        float moveX = Input.GetAxis("Horizontal") * speed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        moveY *= Time.deltaTime;
+        //rotation *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(moveX, moveY, 0);
+
+        // Rotate around our y-axis
+        //transform.Rotate(0, rotation, 0);
+	}
+}
---
+++ Assets/pERSONAL/Pergunta1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pergunta1 : MonoBehaviour {
+
+    public float levelStartDelay = 2f;
+    public float restartLevelDelay = 1f;
+
+    public void Resposta1() {
+        Invoke("Restart", restartLevelDelay);
+    }
+
+    //Restart reloads the scene when called.
+    private void Restart()
+    {
+        //Load the last scene loaded, in this case Main, the only scene in the game. And we load it in "Single" mode so it replace the existing one
+        //and not load all the scene object in the current scene.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
